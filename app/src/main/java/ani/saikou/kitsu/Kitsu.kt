6e8eb2c3b516afdc1285b8a99e31cdc95faa36bd
Shivam <shivam.sdb2@gmail.com>
2021-12-08 23:10:39 +0530

    added kitsu prints

--- app/src/main/java/ani/saikou/kitsu/Kitsu.kt
+++ app/src/main/java/ani/saikou/kitsu/Kitsu.kt
@@ -1,6 +1,7 @@
 package ani.saikou.kitsu
 
 import ani.saikou.anime.Episode
+import ani.saikou.logger
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.*
 import org.jsoup.Connection.*
@@ -21,21 +22,29 @@ object Kitsu {
     }
 
     fun getKitsuEpisodesDetails(title:String): MutableMap<String,Episode>? {
+        val print = false
+        logger("Kitsu : title=$title",print)
         val query = """{"query":"query{searchAnimeByTitle(first:1,title:\"$title\"){nodes{id titles{localized}episodes(first:2000){nodes{number titles{canonical}description thumbnail{original{url}}}}}}}"}"""
         val result = getKitsuData(query)
+        logger("Kitsu : result=$result",print)
         var arr :  MutableMap<String,Episode>? = null
         val node : JsonElement? = Json.decodeFromString<JsonObject>(result).jsonObject["data"]!!.jsonObject["searchAnimeByTitle"]!!.jsonObject["nodes"]
             if (node!=null){ if (!node.jsonArray.isEmpty()){
+                logger("Kitsu : Not Empty",print)
                 val episodes : JsonElement? = node.jsonArray[0].jsonObject["episodes"]!!.jsonObject["nodes"]
+                logger("Kitsu : episodes=$episodes",print)
                 arr = mutableMapOf()
                 episodes?.jsonArray?.forEach {
+                    logger("Kitsu : forEach=$it",print)
                     if (it!=JsonNull){
-                        arr[it.jsonObject["number"]?.toString()?.replace("\"", "")!!] = Episode(
+                        val i = it.jsonObject["number"]?.toString()?.replace("\"", "")!!
+                        arr[i] = Episode(
                             number = it.jsonObject["number"]?.toString()?.replace("\"", "")!!,
                             title = if (it.jsonObject["titles"]!!.jsonObject["canonical"]!=JsonNull) it.jsonObject["titles"]!!.jsonObject["canonical"]?.toString()?.replace("\"", "") else null,
                             desc = if (it.jsonObject["description"]!!.jsonObject["en"]!=JsonNull) it.jsonObject["description"]!!.jsonObject["en"]?.toString()?.replace("\"", "")?.replace("\\n", "\n") else null,
                             thumb = if (it.jsonObject["thumbnail"]!=JsonNull) "https://image-compression-api.herokuapp.com/?q=" + it.jsonObject["thumbnail"]!!.jsonObject["original"]!!.jsonObject["url"]?.toString()?.replace("\"", "") else null,
                         )
+                        logger("Kitsu : arr[$i] = ${arr[i]}",print)
                     }
                 }
             }
